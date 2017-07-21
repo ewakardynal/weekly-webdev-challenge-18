e5c807a99c7e57413210f9bab9eb0c77a659d62f
Ewa <ewa19930814@gmail.com>
2017-07-21 14:42:14 +0200

    Added responsive page navigation

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var hamburger = document.querySelector("#nav-icon");
+hamburger.addEventListener("click", function() {
+   document.querySelector(".page-header").classList.toggle("nav-opened");
+   document.querySelector("#nav-icon").classList.toggle("open");
+}, false);
